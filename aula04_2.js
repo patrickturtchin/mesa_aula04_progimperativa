55c6d396688940f2c24d91213285af8139bc94ca
Patrick Turtchin <patrick.turtchin@gmail.com>
2022-05-09 20:47:29 -0300

    commit do ex

---
+++ aula04_2.js
@@ -0,0 +1,10 @@
+let nome = "Tuzão";
+let idade = 30;
+let usuarioAtivo = true;
+const CPF = "409.614.900-07";
+
+
+console.log(nome);
+console.log(idade);
+console.log(usuarioAtivo);
+console.log(CPF);
---
+++ mesa.js
@@ -0,0 +1,32 @@
+let nome_cliente1 = "José da Silva";
+let idade_cliente1 = 27;
+let peso_cliente1 = 83.5;
+let altura_cliente1 = 1.70;
+let plano_cliente1 = true;
+let imc_cliente1 = peso_cliente1 /  Math.pow(altura_cliente1, 2);
+
+let nome_cliente2 = "Carlos de Souza";
+let idade_cliente2 = 28;
+let peso_cliente2 = 80.1;
+let altura_cliente2 = 1.76;
+let plano_cliente2 = true;
+let imc_cliente2 = peso_cliente2 / Math.pow(altura_cliente2, 2);
+
+let nome_cliente3 = "Aline Ferreira";
+let idade_cliente3 = 33;
+let peso_cliente3 = 63.70;
+let altura_cliente3 = 1.53;
+let plano_cliente3 = true;
+let imc_cliente3 = peso_cliente3 /  Math.pow(altura_cliente3, 2);
+
+let nome_cliente4 = "Ana Paula";
+let idade_cliente4 = 26;
+let peso_cliente4 = 55;
+let altura_cliente4 = 1.62;
+let plano_cliente4 = true;
+let imc_cliente4 = peso_cliente4 /  Math.pow(altura_cliente4, 2);
+
+console.log(nome_cliente1 + " tem " + idade_cliente1 + " anos e seu índice de massa corporal é de " + imc_cliente1.toFixed(2));
+console.log(nome_cliente2 + " tem " + idade_cliente2 + " anos e seu índice de massa corporal é de " + imc_cliente2.toFixed(2));
+console.log(nome_cliente3 + " tem " + idade_cliente3 + " anos e seu índice de massa corporal é de " + imc_cliente3.toFixed(2));
+console.log(nome_cliente4 + " tem " + idade_cliente4 + " anos e seu índice de massa corporal é de " + imc_cliente4.toFixed(2));
